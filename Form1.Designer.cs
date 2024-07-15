d8f9dd1753eba52d52454d54565d141667b74f64
JacobTurner1801 <jacobmturner@gmail.com>
2024-07-15 14:26:59 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Basic_Paint_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            pictureBox6 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(panel2);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1534, 1071);
+            panel1.TabIndex = 0;
+            panel1.MouseDown += panel1_MouseDown;
+            panel1.MouseMove += panel1_MouseMove;
+            panel1.MouseUp += panel1_MouseUp;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(pictureBox6);
+            panel2.Controls.Add(pictureBox5);
+            panel2.Controls.Add(pictureBox4);
+            panel2.Controls.Add(pictureBox3);
+            panel2.Controls.Add(pictureBox2);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(1029, 12);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(502, 155);
+            panel2.TabIndex = 0;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = Color.Gray;
+            pictureBox6.Location = new Point(407, 62);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(53, 50);
+            pictureBox6.TabIndex = 5;
+            pictureBox6.TabStop = false;
+            pictureBox6.Click += pictureBox1_Click;
+            pictureBox6.MouseDown += panel1_MouseDown;
+            pictureBox6.MouseMove += panel1_MouseMove;
+            pictureBox6.MouseUp += panel1_MouseUp;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = Color.Blue;
+            pictureBox5.Location = new Point(265, 62);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(53, 50);
+            pictureBox5.TabIndex = 4;
+            pictureBox5.TabStop = false;
+            pictureBox5.Click += pictureBox1_Click;
+            pictureBox5.MouseDown += panel1_MouseDown;
+            pictureBox5.MouseMove += panel1_MouseMove;
+            pictureBox5.MouseUp += panel1_MouseUp;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.Black;
+            pictureBox4.Location = new Point(105, 62);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(53, 50);
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
+            pictureBox4.Click += pictureBox1_Click;
+            pictureBox4.MouseDown += panel1_MouseDown;
+            pictureBox4.MouseMove += panel1_MouseMove;
+            pictureBox4.MouseUp += panel1_MouseUp;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.FromArgb(192, 0, 192);
+            pictureBox3.Location = new Point(335, 62);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(52, 50);
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox1_Click;
+            pictureBox3.MouseDown += panel1_MouseDown;
+            pictureBox3.MouseMove += panel1_MouseMove;
+            pictureBox3.MouseUp += panel1_MouseUp;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.FromArgb(0, 192, 0);
+            pictureBox2.Location = new Point(184, 62);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(56, 50);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox1_Click;
+            pictureBox2.MouseDown += panel1_MouseDown;
+            pictureBox2.MouseMove += panel1_MouseMove;
+            pictureBox2.MouseUp += panel1_MouseUp;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Red;
+            pictureBox1.Location = new Point(31, 62);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(53, 50);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(980, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(43, 23);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "Reset";
+            textBox1.Click += textBox1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1534, 1071);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+namespace Basic_Paint_App
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;
+        int x, y = -1;
+        bool moving = false;
+        Pen pen;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = panel1.CreateGraphics();
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            pen = new Pen(Color.Black, 7.0f);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+            pen.Color = p.BackColor;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            moving = true;
+            x = e.X; y = e.Y;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (moving && x != -1 && y != -1)
+            {
+                g.DrawLine(pen, new Point(x, y), e.Location);
+                x = e.X;
+                y = e.Y;
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            moving = false;
+            x = -1;
+            y = -1;
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            g.Clear(BackColor);
+        }
+    }
+}
